9a7dc6e371de2bd0befc8dec6a5e99e79c452e71
AndersonboerUlbra <andersonboer@rede.ulbra.br>
2023-08-25 14:27:07 -0300

    Commit inicial da pasta aula3

---
+++ view/EstudanteView.php
@@ -0,0 +1,43 @@
+<?php $listaEstudantes = $_REQUEST["estudantes"]; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudantes</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-dark">
+        <img class="w-40 d-block mx-auto border border-primary border-3 mt-5" src="imagem1.png" alt="conjunto de imagens cartooniando que representam afoordances simbolizando acessibilidade">
+    <div class="container mb-5 mt-5 bg-primary ">
+
+    <table class="table table-bordered table-striped table-dark border border-primary">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nome</th>
+      <th scope="col">Idade</th>
+    </tr>
+  </thead>
+  <tbody>
+    
+    
+    <?php foreach ($listaEstudantes as $estudante) { ?>
+           <tr>
+                
+                <td><?php echo $estudante["id"]; ?></td>
+                <td><?php echo $estudante["nome"]; ?></td>
+                <td><?php echo $estudante["idade"]; ?></td>
+           </tr>
+    
+        <?php } ?>
+  </tbody>
+</table>
+
+
+    </div>
+    </div>
+</body>
+</html>
